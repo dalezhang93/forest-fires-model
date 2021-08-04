a642a5384a7d147a5da4c01ba41e8c62b99f6f6a
zhangduo <zhangduo914@gmail.com>
2021-08-04 17:11:42 +0800

    fix 参数无法正常传递

--- src/main/java/com/example/forestfires/domain/FireCondition.java
+++ src/main/java/com/example/forestfires/domain/FireCondition.java
@@ -1,5 +1,6 @@
 package com.example.forestfires.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -10,9 +11,11 @@ import lombok.Data;
 public class FireCondition {
     private String simulatedtime;
     // 速度计算 R0 = aT + bW － c, T 为温度( ℃ ) ; W 为风力( 级)
+    @JsonProperty("T")
     private Double T;
     private Double a;
     private Double b;
+    @JsonProperty("W")
     private Integer W;
     private Double c;
     // 风向角度
